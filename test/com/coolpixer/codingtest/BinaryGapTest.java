6b0b5fb7edc0f513153c24818ba31586d2df722d
coolpixer <coolpixer@naver.com>
2021-03-24 04:32:51 +0900

    처음 작성
    - BaseSolution으로 공통 코드 적용

---
+++ test/com/coolpixer/codingtest/BinaryGapTest.java
@@ -0,0 +1,62 @@
+package com.coolpixer.codingtest;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryGapTest {
+    private BinaryGap binaryGap;
+
+    public BinaryGapTest() {
+        binaryGap = getBinaryGap();
+    }
+
+    @BeforeAll
+    static void setup() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+
+    BinaryGap getBinaryGap() {
+        return new BinaryGap();
+    }
+
+    @Test()
+    void whenNegativeValueExpectedExceptionThrown() {
+        Exception exception = assertThrows(Exception.class, () -> {
+            binaryGap.solution(-1);
+        });
+
+        String expectedMessage = "invalid parameter";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void whenExpectedZeroReturned() throws Exception {
+        int result = 0;
+
+        result = binaryGap.solution(0b0000_0000_0000_0001);
+        assertEquals(result, 0);
+
+        result = binaryGap.solution(0b1000_0000_0000_0000);
+        assertEquals(result, 0);
+    }
+
+    @Test
+    void whenExpectedPositiveValueReturned() throws Exception {
+        int result = 0;
+
+        result = binaryGap.solution(0b1000_0000_0000_0010);
+        assertEquals(result, 13);
+
+        result = binaryGap.solution(0b1000_1000_0010_0010);
+        assertEquals(result, 5);
+    }
+
+}
